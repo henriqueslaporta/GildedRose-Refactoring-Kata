369e4e8ff3df512eb9930e0d8e9929c27795f8b9
Henrique La Porta <30013138+henriqueslaporta@users.noreply.github.com>
2025-05-18 19:25:30 +0200

    Refactor - Create `increaseQuality` and `decreaseQuality` to remove duplicated validations

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,25 +16,16 @@ class GildedRose {
 
             if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                decreaseQuality(item);
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                increaseQuality(item);
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 10) {
+                        increaseQuality(item);
+                    }
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 10) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 5) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                    if (item.sellIn < 5) {
+                        increaseQuality(item);
                     }
                 }
             }
@@ -42,21 +33,28 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
+                        decreaseQuality(item);
                     } else {
                         item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             }
         }
     }
 
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
     private boolean isALegendaryItem(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
